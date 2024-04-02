a278a5c6969783eb245595645a4f8b82e8acf82e
Hibjul Ahmed <hibjulahmed@gmail.com>
2024-04-03 01:16:22 +0530

    Removed warnings

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,7 +9,7 @@ import "react-vertical-timeline-component/style.min.css";
 
 import { styles } from "../styles";
 import { experiences } from "../constants";
-import { SectionWrapper } from "../hoc";
+import { SectioamnWrapper } from "../hoc";
 import { textVariant } from "../utils/motion";
 
 const ExperienceCard = ({ experience }) => {
