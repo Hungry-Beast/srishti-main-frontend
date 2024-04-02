91e3ba08ff6e5f3048a6ffb1a2f8d38cd4a20c58
Hibjul Ahmed <hibjulahmed@gmail.com>
2024-04-03 01:25:30 +0530

    Removed error

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,7 +9,6 @@ import "react-vertical-timeline-component/style.min.css";
 
 import { styles } from "../styles";
 import { experiences } from "../constants";
-import { SectioamnWrapper } from "../hoc";
 import { textVariant } from "../utils/motion";
 
 const ExperienceCard = ({ experience }) => {
