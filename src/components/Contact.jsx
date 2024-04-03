5c348824b761cee4e61e5bd6f67f71897e8827a0
Subu Richo <suburicho@gmail.com>
2024-04-04 01:13:23 +0530

    changes in Login/Registration page  frontend

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -58,14 +58,15 @@ const Contact = () => {
       setLoading(false);
     }
   };
-
+//bg-black-100
   return (
-    <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
-      <motion.div variants={slideIn("left", "tween", 0.2, 1)} className='flex-[0.75] bg-black-100 p-8 rounded-2xl'>
-        <p className={styles.sectionSubText}>Shrist 2k24</p>
+    <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden `}>
+      <motion.div variants={slideIn("left", "tween", 0.2, 1)} className='flex-[0.75]  p-8 rounded-2xl backdrop-blur-sm border-2 border-gray-700 bg-gray-800/70'>
+      
+        <p className={styles.sectionSubText}>Shristi 2k24</p>
         <h3 className={styles.sectionHeadText}>Login.</h3>
 
-        <form ref={formRef} onSubmit={handleSubmit} className='mt-12 flex flex-col gap-8'>
+        <form ref={formRef} onSubmit={handleSubmit} className=' mt-6 flex flex-col gap-4  '>
           <label className='flex flex-col'>
             <span className='text-white font-medium mb-4'>Registration Number</span>
             <input
@@ -74,7 +75,7 @@ const Contact = () => {
               value={formData.regNo}
               onChange={handleChange}
               placeholder='Enter your registration number'
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              className='backdrop-blur-sm border-2 border-gray-700 bg-gray-800/70  py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium '
             />
           </label>
           <label className='flex flex-col'>
@@ -85,26 +86,25 @@ const Contact = () => {
               value={formData.password}
               onChange={handleChange}
               placeholder='Enter your password'
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              className='backdrop-blur-sm border-gray-700 bg-gray-800/70 py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
-          <button className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary' type="submit" disabled={loading}>
-            {loading ? "Logging in..." : "Login"}
-          </button>
+            <button className='bg-[#915EFF] py-3 px-8 rounded-xl outline-none w-fit text-white font-bold' type="submit" disabled={loading}>
+              {loading ? "Logging in..." : "Login"}
+            </button>
 
-      <Link to="/register"> {/* Link to the register page */}
-        <button 
-          id="newUserRegistration"
-          type='submit'
-          className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
-          
-        >
-          New User?
-        </button>
-      </Link>
-        </form>
+            <Link to="/register">
+              <span 
+                id="newUserRegistration"
+                type='submit'
+                className='text-white underline decoration-solid'
+              >
+                Don't have an account? Register here
+              </span>
+            </Link>
+      </form>
       </motion.div>
-      <motion.div variants={slideIn("right", "tween", 0.2, 1)} className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'>
+      <motion.div variants={slideIn("right", "tween", 0.2, 1)} className='hidden md:block xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'>
         <EarthCanvas />
       </motion.div>
     </div>
--- src/components/RegisterUser.jsx
+++ src/components/RegisterUser.jsx
@@ -7,15 +7,18 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion"; // Assuming you have slideIn imported correctly
 
+import Switch from "react-switch";
+
 const RegisterUser = () => {
   const [form, setForm] = useState({
     name: "",
     email: "",
     password: "",
     isNeristian: "", // Added isNeristian to the initial state
-    regNo: "" // Added regNo to the initial state
+    regNo: "", // Added regNo to the initial state
   });
   const [loading, setLoading] = useState(false);
+  const [checked, setChecked] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -28,26 +31,29 @@ const RegisterUser = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
-    
+
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-  
+
     const raw = JSON.stringify({
-      "name": form.name,
-      "password": form.password,
-      "phoneNo": form.phoneNo, // Assuming you have this field in your form state
-      "regNo": form.regNo,
-      "email":form.email
+      name: form.name,
+      password: form.password,
+      phoneNo: form.phoneNo, // Assuming you have this field in your form state
+      regNo: form.regNo,
+      email: form.email,
     });
-  
+
     const requestOptions = {
       method: "POST",
       headers: myHeaders,
       body: raw,
-      redirect: "follow"
+      redirect: "follow",
     };
-  
-    fetch("https://shristi-backend.azurewebsites.net/auth/createUser", requestOptions)
+
+    fetch(
+      "https://shristi-backend.azurewebsites.net/auth/createUser",
+      requestOptions
+    )
       .then((response) => response.json())
       .then((result) => {
         console.log(result); // Log the response from the server
@@ -60,83 +66,106 @@ const RegisterUser = () => {
         // Handle error, such as displaying an error message to the user
       });
   };
+  console.log(form.isNeristian);
 
   return (
-    <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
+    <div
+      className={`xl:mt-8 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+    >
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
-        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+        className="flex-[0.75] p-8 rounded-2xl backdrop-blur-sm border-2 border-gray-700 bg-gray-800/70"
       >
-        <p className={styles.sectionSubText}>Shrist 2k24</p>
+        <p className={styles.sectionSubText}>Shristi 2k24</p>
         <h3 className={styles.sectionHeadText}>Register.</h3>
 
-        <form onSubmit={handleSubmit} className='mt-12 flex flex-col gap-8'>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Name</span>
+        <form onSubmit={handleSubmit} className="mt-6 flex flex-col gap-4">
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
             <input
-              type='text'
-              name='name'
+              type="text"
+              name="name"
               value={form.name}
               onChange={handleChange}
               placeholder="Enter your name"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              className="backdrop-blur-sm border-gray-700 bg-gray-800/70 py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Email</span>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Email</span>
             <input
-              type='email'
-              name='email'
+              type="email"
+              name="email"
               value={form.email}
               onChange={handleChange}
               placeholder="Enter your email address"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              className="backdrop-blur-sm border-gray-700 bg-gray-800/70 py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
-          <label className='flex items-center justify-between'>
-            <span className='text-white font-medium  w-max'>Are you a Neristian?</span>
-            <select name='isNeristian' value={form.isNeristian} onChange={handleChange} className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium ml-0 mt-0'>
-              <option value='' className="text-center">Select</option>
-              <option value='yes'>Yes</option>
-              <option value='no'>No</option>
-            </select>
+          <label className="flex items-center justify-between">
+            <span className="text-white font-medium  w-max">
+              Are you a Neristian?
+            </span>
+            <Switch
+              name="isNeristian"
+              value={form.isNeristian}
+              onChange={(e) => {
+                setChecked(e);
+                setForm({
+                  ...form,
+                  ["isNeristian"]: e,
+                });
+              }}
+              checked={checked}
+              className="bg-black-100"
+              onColor="#A77EFF"
+              onHandleColor="#915EFF"
+              required
+            />
           </label>
-          {form.isNeristian === 'yes' && (
-            <label className='flex flex-col'>
-              <span className='text-white font-medium mb-4'>Registration Number</span>
+          {form.isNeristian && (
+            <label className="flex flex-col">
+              <span className="text-white font-medium mb-4">
+                Registration Number
+              </span>
               <input
-                type='text'
-                name='regNo'
+                type="text"
+                name="regNo"
                 value={form.regNo}
                 onChange={handleChange}
-                placeholder='Enter your registration number'
-                className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+                placeholder="Enter your registration number"
+                className="backdrop-blur-sm border-gray-700 bg-gray-800/70 py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
               />
             </label>
           )}
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Password</span>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Password</span>
             <input
-              type='password'
-              name='password'
+              type="password"
+              name="password"
               value={form.password}
               onChange={handleChange}
               placeholder="Enter your password"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              className="backdrop-blur-sm border-gray-700 bg-gray-800/70 py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
 
           <button
-            type='submit'
-            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+            type="submit"
+            className="bg-[#915EFF] py-3 px-8 rounded-xl outline-none w-fit text-white font-bold "
           >
             Register User
           </button>
+          <Link to="/login">
+            <span className="text-white underline decoration-solid">
+              Already a user?Login here!
+            </span>
+          </Link>
         </form>
       </motion.div>
       <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
-        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+        className="hidden md:block xl:flex-1 xl:w-auto md:w-full"
       >
         <EarthCanvas />
       </motion.div>
